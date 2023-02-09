2a602d265c7092051680c0302125ef9d2d0e618e
Yerassyl Tleugazy <yerassyl@remofirst.com>
2023-02-10 00:41:30 +0600

    sdsddsdsds

--- main/applications/models.py
+++ main/applications/models.py
@@ -7,78 +7,75 @@ class Application(Timestamped):
     id = models.CharField(primary_key=True, max_length=500)
     app_type = models.CharField(
         choices=ProblemType.choices,
-        max_length=255
+        max_length=255, default="", null=True, blank=True,
     )
     app_status = models.CharField(
         choices=Status.choices,
         max_length=255,
+        default="", null=True, blank=True,
     )
-    message = models.TextField(
-    )
+    message = models.TextField(default="", null=True, blank=True,
+                               )
     first_name = models.CharField(
-        max_length=255
+        max_length=255, default="", null=True, blank=True,
     )
     last_name = models.CharField(
-        max_length=255
+        max_length=255, default="", null=True, blank=True,
     )
     patronymic = models.CharField(
-        max_length=255
+        max_length=255, default="", null=True, blank=True,
     )
     phone_number = models.CharField(
-        max_length=255
+        max_length=255, default="", null=True, blank=True,
     )
     photo_url = models.CharField(
-        max_length=500
+        max_length=500, default="", null=True, blank=True,
     )
     video_url = models.CharField(
-        max_length=500
+        max_length=500, default="", null=True, blank=True,
     )
     address = models.CharField(
-        max_length=600
+        max_length=600, default="", null=True, blank=True,
     )
-    longitude = models.CharField(
-        max_length=255
-    )
-    latitude = models.CharField(
-        max_length=300
-    )
+    longitude = models.DecimalField(decimal_places=10, max_digits=100, default=0.0)
+    latitude = models.DecimalField(decimal_places=10, max_digits=100, default=0.0)
     user_id = models.CharField(
-        max_length=400
+        max_length=400, default="", null=True, blank=True,
     )
     created_date = models.CharField(
-        max_length=400
+        max_length=400, default="", null=True, blank=True,
     )
 
 
 class News(Timestamped):
     id = models.CharField(primary_key=True, max_length=500)
-    title = models.CharField(max_length=500)
-    small_description = models.CharField(max_length=500)
+    title = models.CharField(max_length=500, default="")
+    small_description = models.CharField(max_length=500, default="")
     description = models.TextField()
-    photo_url = models.CharField(max_length=600)
-    author_id = models.CharField(max_length=600)
+    photo_url = models.CharField(max_length=600, default="")
+    author_id = models.CharField(max_length=600, default="")
     created_date = models.CharField(
-        max_length=400
+        max_length=400, default=""
     )
 
 
 class Event(Timestamped):
     id = models.CharField(primary_key=True, max_length=500)
-    address = models.CharField(max_length=500)
+    address = models.CharField(max_length=500, default="")
     description = models.TextField()
     date = models.CharField(
-        max_length=400
+        max_length=400, default=""
     )
     time = models.CharField(
-        max_length=400
+        max_length=400, default=""
     )
-    organizer_info = models.CharField(max_length=400)
-    document_url = models.CharField(max_length=600)
-    longitude = models.DecimalField(decimal_places=6, max_digits=100)
-    latitude = models.DecimalField(decimal_places=6, max_digits=100)
+    organizer_info = models.CharField(max_length=400, default="")
+    document_url = models.CharField(max_length=600, default="")
+    longitude = models.DecimalField(decimal_places=10, max_digits=100, default=0.0)
+    latitude = models.DecimalField(decimal_places=10, max_digits=100, default=0.0)
     user_id = models.CharField(
-        max_length=400
+        max_length=400, default=""
     )
     created_date = models.CharField(
-        max_length=400
+        max_length=400, default=""
     )
--- main/applications/urls.py
+++ main/applications/urls.py
@@ -4,7 +4,7 @@ from django.urls import path
 from rest_framework.routers import DefaultRouter
 
 from .views import (
-    ListApplicationsApi, CreateApplicationApi
+    ListApplicationsApi, CreateApplicationApi, UpdateApplicationApi
 )
 
 app_name = "applications"
@@ -13,7 +13,9 @@ urlpatterns: Tuple = router.urls
 
 applications_patterns: Tuple[str, ...] = (
     path("applications/list/", ListApplicationsApi.as_view(), name="application-list"),
-    path("applications/create/", CreateApplicationApi.as_view(), name="application-create")
+    path("applications/create/", CreateApplicationApi.as_view(), name="application-create"),
+    path("applications/update/", UpdateApplicationApi.as_view(), name="application-update"),
+
 )
 
 urlpatterns += (
