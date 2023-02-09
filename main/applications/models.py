a78ec0a129eb9e25c7be1bf8a3eb319414eec181
Yerassyl Tleugazy <yerassyl@remofirst.com>
2023-02-10 01:30:32 +0600

    sdsddsd

--- main/applications/models.py
+++ main/applications/models.py
@@ -7,43 +7,43 @@ class Application(Timestamped):
     id = models.CharField(primary_key=True, max_length=500)
     app_type = models.CharField(
         choices=ProblemType.choices,
-        max_length=255, default="", null=True, blank=True,
+        max_length=255, default="",
     )
     app_status = models.CharField(
         choices=Status.choices,
         max_length=255,
         default="", null=True, blank=True,
     )
-    message = models.TextField(default="", null=True, blank=True,
+    message = models.TextField(default="",
                                )
     first_name = models.CharField(
-        max_length=255, default="", null=True, blank=True,
+        max_length=255, default="",
     )
     last_name = models.CharField(
-        max_length=255, default="", null=True, blank=True,
+        max_length=255, default="",
     )
     patronymic = models.CharField(
-        max_length=255, default="", null=True, blank=True,
+        max_length=255, default="",
     )
     phone_number = models.CharField(
-        max_length=255, default="", null=True, blank=True,
+        max_length=255, default="",
     )
     photo_url = models.CharField(
-        max_length=500, default="", null=True, blank=True,
+        max_length=500, default="",
     )
     video_url = models.CharField(
-        max_length=500, default="", null=True, blank=True,
+        max_length=500, default="",
     )
     address = models.CharField(
-        max_length=600, default="", null=True, blank=True,
+        max_length=600, default="",
     )
     longitude = models.DecimalField(decimal_places=10, max_digits=100, default=0.0)
     latitude = models.DecimalField(decimal_places=10, max_digits=100, default=0.0)
     user_id = models.CharField(
-        max_length=400, default="", null=True, blank=True,
+        max_length=400, default="",
     )
     created_date = models.CharField(
-        max_length=400, default="", null=True, blank=True,
+        max_length=400, default="",
     )
 
 
@@ -51,7 +51,7 @@ class News(Timestamped):
     id = models.CharField(primary_key=True, max_length=500)
     title = models.CharField(max_length=500, default="")
     small_description = models.CharField(max_length=500, default="")
-    description = models.TextField()
+    description = models.TextField(default="")
     photo_url = models.CharField(max_length=600, default="")
     author_id = models.CharField(max_length=600, default="")
     created_date = models.CharField(
@@ -62,8 +62,8 @@ class News(Timestamped):
 class Event(Timestamped):
     id = models.CharField(primary_key=True, max_length=500)
     address = models.CharField(max_length=500, default="")
-    description = models.TextField()
-    date = models.CharField(
+    description = models.TextField(default="")
+    date = models.DateField(
         max_length=400, default=""
     )
     time = models.CharField(
@@ -76,6 +76,6 @@ class Event(Timestamped):
     user_id = models.CharField(
         max_length=400, default=""
     )
-    created_date = models.CharField(
+    created_date = models.DateField(
         max_length=400, default=""
     )
